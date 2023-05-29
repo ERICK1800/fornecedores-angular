15f648c51feac2b4663c9aaecef5fed9bc2b9aa7
ERICK1800 <ericklimaflorencio@gmail.com>
2023-05-29 19:18:58 -0300

    form and server

--- src/app/fornec.service.ts
+++ src/app/fornec.service.ts
@@ -1,9 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Fornec } from './fornecedores';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FornecService {
 
-  constructor() { }
+  url = "http://localhost:3000/fornecedores";
+
+  constructor(private http: HttpClient) { }
+
+  getFornec(): Observable<Fornec[]>{
+    return this.http.get<Fornec[]>(this.url);
+  }
+  saveFornec(cliente: Fornec): Observable<Fornec>{
+    return this.http.post<Fornec>(this.url, cliente);
+  }
+
+  delFornec(cliente: Fornec): Observable<void>{
+    return this.http.delete<void>(this.url + "/" + cliente.id);
+  }
+
+  updateFornec(cliente: Fornec): Observable<Fornec>{
+    return this.http.put<Fornec>(this.url + "/" + cliente.id, cliente);
+  }
 }
--- src/app/fornec/fornec.component.ts
+++ src/app/fornec/fornec.component.ts
@@ -1,10 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { FornecService } from '../fornec.service';
+import { Fornec } from '../fornecedores';
 
 @Component({
   selector: 'app-fornec',
   templateUrl: './fornec.component.html',
   styleUrls: ['./fornec.component.css']
 })
-export class FornecComponent {
+export class FornecComponent implements OnInit{
+
+  fornecedores: Fornec[] = [];
+  formGroupForncedores: FormGroup;
+  isEditing: boolean = false;
+
+  constructor(private fornecService: FornecService, private formBuilder: FormBuilder){
+    this.formGroupForncedores = formBuilder.group({
+      id: [''],
+      name: [''],
+      active: [false],
+      category: [''],
+      contact: ['']
+    })
+  }
+
+  ngOnInit(): void {
+    this.LoadFornec();
+  }
+
+  LoadFornec(){
+    this.fornecService.getFornec().subscribe(
+      {
+        next: data => this.fornecedores = data,
+        error: msg => console.log("Erro ao chamar o endpont " + msg)
+      });
+  }
+
+  save(){
+    if(this.isEditing){
+      this.fornecService.updateFornec(this.formGroupForncedores.value).subscribe({
+        next: () => {
+          this.LoadFornec();
+          this.formGroupForncedores.reset();
+          this.isEditing = false;
+        }
+      })
+    } else{
+      this.fornecService.saveFornec(this.formGroupForncedores.value).subscribe({
+        next: data => {
+          this.fornecedores.push(data);
+          this.formGroupForncedores.reset();
+        }
+      })
+    }
+  }
+
+  remove(fornec: Fornec): void{
+    this.fornecService.delFornec(fornec).subscribe({
+      next: () => this.LoadFornec()
+    })
+  }
+
+  edit(fornec: Fornec): void{
+    this.formGroupForncedores.setValue(fornec);
+    this.isEditing = true;
+  }
 
 }
